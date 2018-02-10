49204906a837e86fc85b58ce2c24ab8469d9ec92
Anthilam <timtim300@gmail.com>
2018-02-10 17:02:40 +0100

    Génération tableau finie + retouches CSS

--- public/js/jeuDuo.js
+++ public/js/jeuDuo.js
@@ -1,110 +1,186 @@
+// A class representing a robot
 class Robot {
   constructor() {
-    this.top = 0;
-    this.left = 0;
+    this.row = 0;
+    this.cell = 0;
     this.html;
   }
 
   setPos(offsetTop, offsetLeft) {
-    this.top = offsetTop;
-    this.left = offsetLeft;
     this.html.style.top = offsetTop + "px";
     this.html.style.left = offsetLeft + "px";
   }
 }
 
+// A class representing a flag
 class Flag {
   constructor() {
-    this.top = 0;
-    this.left = 0;
+    this.row = 0;
+    this.cell = 0;
     this.html;
   }
 
   setPos(offsetTop, offsetLeft) {
-    this.top = offsetTop;
-    this.left = offsetLeft;
     this.html.style.top = offsetTop + "px";
     this.html.style.left = offsetLeft + "px";
   }
 }
 
+// Global vars
 var blueRobot = new Robot();
 var redRobot = new Robot();
-
+// A table containing the flags
 var tabFlag = [];
+// A boolean table representing which cell of the board is occupied or not
+var tabBoard = [[],[],[],[],[],[],[],[],[]];
 
+/* refreshPos():
+ *  Refreshes all the elements position
+ */
+function refreshPos() {
+  console.log("Call: refreshPos()");
+
+  var board = document.getElementById("board");
+  // Refresh of the redRobot
+  var cell = board.rows[redRobot.row].cells[redRobot.cell];
+  redRobot.setPos(cell.offsetTop, cell.offsetLeft);
+  // Refresh of the blueRobot
+  cell = board.rows[blueRobot.row].cells[blueRobot.cell];
+  blueRobot.setPos(cell.offsetTop, cell.offsetLeft);
+  // Refresh of the flags
+  for (var i in tabFlag) {
+    cell = board.rows[tabFlag[i].row].cells[tabFlag[i].cell];
+    tabFlag[i].setPos(cell.offsetTop, cell.offsetLeft);
+  }
+}
+
+/* randInt(max):
+ *  Randomizes a number between 0 and max-1
+ */
+function randInt(max) {
+  console.log("Call: randInt("+max+")");
+
+  return Math.floor(Math.random() * max);
+}
+
+/* superSwitch(elem, rand, row, cell, other, onside) :
+ *  Makes a special switch with the parameters given
+ */
+function superSwitch(elem, rand, row, cell, other, onside) {
+  console.log("Call: superSwitch("+elem+","+rand+","+row+","+cell+","+other+","+onside+")");
+
+  if (onside) {
+    switch (rand) {
+      case 0: elem.row = row; elem.cell = cell; break;
+      case 1: elem.row = row+1; elem.cell = cell; break;
+      case 2: elem.row = row+2; elem.cell = cell; break;
+      case 3: elem.row = row+1; elem.cell = other; break;
+      default: elem.row = row+1; elem.cell = cell;
+    }
+  }
+  else {
+    switch (rand) {
+      case 0: elem.row = row; elem.cell = cell; break;
+      case 1: elem.row = row; elem.cell = cell+1; break;
+      case 2: elem.row = row; elem.cell = cell+2; break;
+      case 3: elem.row = other; elem.cell = cell+1; break;
+      default: elem.row = row; elem.cell = cell+1;
+    }
+  }
+}
+
+/* init():
+ *  Initializes the game
+ */
 function init() {
+  console.log("--INIT--");
+
   var board = document.getElementById("board");
   var board_container = document.getElementById("board_container");
+  var r = 0; // Random number
 
-  // Génération du tableau
+  // Board generation
   for (var i = 0; i < 9; ++i) {
     var row = document.createElement("tr");
     board.appendChild(row);
     for (var j = 0; j < 9; ++j) {
       var cell = document.createElement("td");
+      // Blue and Red bases generation
+      if (i == 3 || i == 4 || i == 5) {
+        if (j == 0 || (j == 1 && i == 4)) {
+          cell.setAttribute("class", "redBase");
+        }
+        else if (j == 8 || (j == 7 && i == 4)) {
+          cell.setAttribute("class", "blueBase");
+        }
+      }
+      // Flag bases generation
+      else if (j == 3 || j == 4 || j == 5) {
+        if (i == 0 || (i == 1 && j == 4) || (i == 7 && j == 4) || i == 8) {
+          cell.setAttribute("class", "flagBase");
+        }
+      }
       row.appendChild(cell);
+      // Initialization of the boolean board
+      tabBoard[i][j] = false;
     }
   }
 
-  // Génération des robots
-  blueRobot.html = document.createElement("div");
-  var att = document.createAttribute("id");
-  att.value = "blueRobot";
-  blueRobot.html.setAttributeNode(att);
-  board_container.appendChild(blueRobot.html);
-
-  var cell = board.rows[4].cells[8];
-  blueRobot.setPos(cell.offsetTop, cell.offsetLeft);
-
+  // redRobot generation
   redRobot.html = document.createElement("div");
-  att = document.createAttribute("id");
-  att.value = "redRobot";
-  redRobot.html.setAttributeNode(att);
+  redRobot.html.setAttribute("id", "redRobot");
   board_container.appendChild(redRobot.html);
+  r = randInt(4);
+  superSwitch(redRobot, r, 3, 0, 1, true);
 
-  cell = board.rows[4].cells[0];
-  redRobot.setPos(cell.offsetTop, cell.offsetLeft);
+  // blueRobot generation
+  blueRobot.html = document.createElement("div");
+  blueRobot.html.setAttribute("id", "blueRobot");
+  board_container.appendChild(blueRobot.html);
+  r = randInt(4);
+  superSwitch(blueRobot, r, 3, 8, 7, true);
 
-  // Génération des drapeaux
+  // Flags generation
   for (var i = 0; i < 8; ++i) {
     tabFlag[i] = new Flag();
     tabFlag[i].html = document.createElement("div");
     att = document.createAttribute("class");
-    if (i == 0 || i == 2 || i == 4 || i == 6) {
-      if (i == 0 || i == 2) {
-        cell = board.rows[0].cells[3+i];
-      }
-      if (i == 4) {
-        cell = board.rows[7].cells[4];
-      }
-      if (i == 6) {
-        cell = board.rows[8].cells[4];
-      }
-      att.value = "redFlag";
+
+    // First base
+    if (i < 4) {
+      // Loop until an empty cell is found
+      do {
+        r = randInt(4);
+        superSwitch(tabFlag[i], r, 0, 3, 1, false);
+      } while (tabBoard[tabFlag[i].row][tabFlag[i].cell] == true);
+
+      tabBoard[tabFlag[i].row][tabFlag[i].cell] = true;
+      (i < 2) ? att.value = "redFlag" : att.value = "blueFlag";
     }
+    // Second base
     else {
-      if (i == 1) {
-        cell = board.rows[0].cells[4];
-      }
-      if (i == 3) {
-        cell = board.rows[1].cells[4];
-      }
-      if (i == 5 || i == 7) {
-        cell = board.rows[8].cells[i-2];
-      }
-      att.value = "blueFlag";
+      // Loop until an empty cell is found
+      do {
+        r = randInt(4);
+        superSwitch(tabFlag[i], r, 8, 3, 7, false);
+      } while (tabBoard[tabFlag[i].row][tabFlag[i].cell] == true);
+
+      tabBoard[tabFlag[i].row][tabFlag[i].cell] = true;
+      (i < 6) ? att.value = "redFlag" : att.value = "blueFlag";
     }
 
     tabFlag[i].html.setAttributeNode(att);
     board_container.appendChild(tabFlag[i].html);
-    tabFlag[i].setPos(cell.offsetTop, cell.offsetLeft);
   }
+
+  refreshPos();
 }
 
 function main() {
-  console.log("MAIN");
+ console.log("--MAIN--");
 }
+
+window.addEventListener("resize", refreshPos);
 
 document.addEventListener("DOMContentLoaded", function(e) {
   init();
