838838f77ee9ba35fe27bb2827f3748f92431329
Anthilam <timtim300@gmail.com>
2018-02-16 18:02:40 +0100

    Retouches CSS/HTML + gestion collision + ajout com

--- public/js/jeuDuo.js
+++ public/js/jeuDuo.js
@@ -1,3 +1,4 @@
+/*---CLASSES------------------------------------------------------------------*/
 // A class representing a robot
 class Robot {
   constructor(c, h) {
@@ -9,18 +10,26 @@ class Robot {
     this.heading = h;
   }
 
+  /* setPos(offsetTop, offsetLeft):
+   *  Sets the HTML position of the robot
+   */
   setPos(offsetTop, offsetLeft) {
     this.html.style.top = offsetTop + "px";
     this.html.style.left = offsetLeft + "px";
   }
 
+  /* rotate(head, dir, color):
+   *  Sets the HTML rotation of the robot
+   */
   rotate(head, dir, color) {
-    var div;
+    var body, tracks;
     if (color == "blue") {
-      div = document.getElementById("blueRobotBody");
+      body = document.getElementById("blueRobotBody");
+      tracks = document.getElementById("blueRobotTracks");
     }
     else {
-      div = document.getElementById("redRobotBody");
+      body = document.getElementById("redRobotBody");
+      tracks = document.getElementById("redRobotTracks");
     }
 
     var angle;
@@ -32,27 +41,30 @@ class Robot {
       default:;
     }
 
-    div.style.transform = "rotate("+angle+"deg)";
+    body.style.transform = "rotate("+angle+"deg)";
+    tracks.style.transform = "rotate("+angle+"deg)";
   }
 
+  /* move(dir):
+   *  Moves the robot in a direction
+   */
   move(dir) {
-    console.log("Call: move("+dir+")");
-
+    tabBoard[this.row][this.cell] = false;
     if (dir == "N") {
-      if (this.row - 1 >= 0) {
+      if (this.row - 1 >= 0 && tabBoard[this.row - 1][this.cell] != true) {
         this.row -= 1;
         this.heading = "N";
         return "north";
       }
     }
     else if (dir == "E") {
-      if (this.cell + 1 <= 8) {
+      if (this.cell + 1 <= 8 && tabBoard[this.row][this.cell + 1] != true) {
         this.cell += 1;
         this.heading = "E";
         return "east";
       }
     }
-    else if (dir == "S") {
+    else if (dir == "S" && tabBoard[this.row + 1][this.cell] != true) {
       if (this.row + 1 <= 8) {
         this.row += 1;
         this.heading = "S";
@@ -60,22 +72,24 @@ class Robot {
       }
     }
     else if (dir == "W") {
-      if (this.cell - 1 >= 0) {
+      if (this.cell - 1 >= 0 && tabBoard[this.row][this.cell - 1] != true) {
         this.cell -= 1;
         this.heading = "W";
         return "west";
       }
     }
-    return "";
+    return "idle";
   }
 
+  /* useCard(n):
+   *  Triggers the effect of a card
+   */
   useCard(n) {
     /* TODO GERER LES COLLISIONS ET SORTIE DE TERRAIN */
 
     console.log("Call: useCard("+this.tabCard[n]+")");
 
     var anim = "";
-
     switch (this.tabCard[n]) {
       case "N": anim = this.move("N"); break;
       case "E": anim = this.move("E"); break;
@@ -89,7 +103,7 @@ class Robot {
       case "UNDO": undo(); break;
       case "X2": x2(); break;
       case "STOP": stop(); break;
-      default:;
+      default: anim = "idle";
     }
 
     this.rotate(this.heading, this.tabCard[n], this.color);
@@ -99,12 +113,16 @@ class Robot {
 
 // A class representing a flag
 class Flag {
-  constructor() {
+  constructor(id) {
     this.row = 0;
     this.cell = 0;
     this.html;
+    this.id = id;
   }
 
+  /* setPos(offsetTop, offsetLeft):
+   *  Sets the position of the flag
+   */
   setPos(offsetTop, offsetLeft) {
     this.html.style.top = offsetTop + "px";
     this.html.style.left = offsetLeft + "px";
@@ -119,12 +137,16 @@ class Card {
     this.html = "../images/"+name+"-"+color+".png";
   }
 
-  reaction(tabCard, i) {
+  /* addTo(tabCard, i):
+   *  Adds the card to a table at the index i
+   */
+  addTo(tabCard, i) {
     tabCard[i] = this.action;
   }
 }
 
-// Global vars
+/*---VARIABLES----------------------------------------------------------------*/
+
 var blueRobot = new Robot("blue", "W");
 var redRobot = new Robot("red", "E");
 // A table containing the flags
@@ -145,21 +167,27 @@ var deck_ind = -1;
 //Var which represent if the player change the red or blue deck
 var isRed = true;
 
+/*---DIVERSE FUNCTIONS---------------------------------------------------------*/
+
 /* refreshPos():
  *  Refreshes all the elements position
  */
 function refreshPos() {
-  console.log("Call: refreshPos()");
-
   var board = document.getElementById("board");
+
   // Refresh of the redRobot
+  tabBoard[redRobot.row][redRobot.cell] = true;
   var cell = board.rows[redRobot.row].cells[redRobot.cell];
   redRobot.setPos(cell.offsetTop, cell.offsetLeft);
+
   // Refresh of the blueRobot
+  tabBoard[blueRobot.row][blueRobot.cell] = true;
   cell = board.rows[blueRobot.row].cells[blueRobot.cell];
   blueRobot.setPos(cell.offsetTop, cell.offsetLeft);
+
   // Refresh of the flags
   for (var i in tabFlag) {
+    tabBoard[tabFlag[i].row][tabFlag[i].cell] = tabFlag[i].id;
     cell = board.rows[tabFlag[i].row].cells[tabFlag[i].cell];
     tabFlag[i].setPos(cell.offsetTop, cell.offsetLeft);
   }
@@ -169,17 +197,13 @@ function refreshPos() {
  *  Randomizes a number between 0 and max-1
  */
 function randInt(max) {
-  console.log("Call: randInt("+max+")");
-
   return Math.floor(Math.random() * max);
 }
 
-/* superSwitch(elem, rand, row, cell, other, onside) :
+/* superSwitch(elem, rand, row, cell, other, onside):
  *  Makes a special switch with the parameters given
  */
 function superSwitch(elem, rand, row, cell, other, onside) {
-  console.log("Call: superSwitch("+elem+","+rand+","+row+","+cell+","+other+","+onside+")");
-
   if (onside) {
     switch (rand) {
       case 0: elem.row = row; elem.cell = cell; break;
@@ -199,6 +223,93 @@ function superSwitch(elem, rand, row, cell, other, onside) {
     }
   }
 }
+
+/* useCards(i, b):
+ *  Uses the cards of the two robots alternatively, "blue" specifies which robot
+ */
+function useCards(i, blue) {
+  if (blue) {
+    blueRobot.useCard(i);
+  }
+  else {
+    redRobot.useCard(i);
+  }
+
+  return !blue;
+}
+
+/* show_deck(deck, id):
+ *
+ */
+function show_deck(deck,id){
+  deck_ind = id;
+  document.getElementById("deck").style.display = "inline-block";
+  document.getElementById("img_deck").innerHTML = "";
+  var html;
+  console.log(deck);
+  if (deck == 0) {
+    isRed = true;
+    html = print_deck(red_deck,temporary_red_hand);
+    document.getElementById("deck").style.backgroundColor = "#db6641";
+    document.getElementById("deck").style.border = "2px solid #a33614";
+  }else{
+    isRed = false;
+    html = print_deck(blue_deck,temporary_blue_hand);
+    document.getElementById("deck").style.backgroundColor = "#418cdb";
+    document.getElementById("deck").style.border = "2px solid #1449a3";
+  }
+  document.getElementById("img_deck").innerHTML = html;
+}
+
+/* print_deck(deck, temporary_hand):
+ *
+ */
+function print_deck(deck,temporary_hand){
+  var html="";
+  for(var i in deck){
+    if(temporary_hand.indexOf(i) == -1){
+        html += '<input type="image" onclick="choose_card(\''+i+'\')" class="show_deck" src="'+deck[i].html+'" alt="'+i+'">';
+    }
+  }
+  return html;
+}
+
+/* choose_card(ind):
+ *
+ */
+function choose_card(ind){
+  var id;
+  var src;
+  var id_text;
+  var action;
+  if(isRed){
+    id = "red_"+deck_ind;
+    id_text = "red_text_"+deck_ind;
+    src = red_deck[ind].html;
+    temporary_red_hand[deck_ind] = ind;
+    document.getElementById("img_deck").innerHTML = "";
+    document.getElementById("img_deck").innerHTML = print_deck(red_deck,temporary_red_hand);
+  }else{
+    id = "blue_"+deck_ind;
+    id_text = "blue_text_"+deck_ind;
+    src = blue_deck[ind].html;
+    temporary_blue_hand[deck_ind] = ind;
+    document.getElementById("img_deck").innerHTML = "";
+    document.getElementById("img_deck").innerHTML = print_deck(blue_deck,temporary_blue_hand);
+  }
+  document.getElementById(id).src = src;
+  document.getElementById(id_text).style.display = "none";
+
+}
+
+/* close_deck():
+ *
+ */
+function close_deck(){
+  document.getElementById("deck").style.display = "none";
+}
+
+/*---MAIN FUNCTIONS-----------------------------------------------------------*/
 
 /* init():
  *  Initializes the game
@@ -243,6 +354,9 @@ function init() {
   var body = document.createElement("div");
   body.setAttribute("id", "redRobotBody");
   redRobot.html.appendChild(body);
+  body = document.createElement("div");
+  body.setAttribute("id", "redRobotTracks");
+  redRobot.html.appendChild(body);
   board_container.appendChild(redRobot.html);
   r = randInt(4);
   superSwitch(redRobot, r, 3, 0, 1, true);
@@ -253,13 +367,16 @@ function init() {
   body = document.createElement("div");
   body.setAttribute("id", "blueRobotBody");
   blueRobot.html.appendChild(body);
+  body = document.createElement("div");
+  body.setAttribute("id", "blueRobotTracks");
+  blueRobot.html.appendChild(body);
   board_container.appendChild(blueRobot.html);
   r = randInt(4);
   superSwitch(blueRobot, r, 3, 8, 7, true);
 
   // Flags generation
   for (var i = 0; i < 8; ++i) {
-    tabFlag[i] = new Flag();
+    tabFlag[i] = new Flag(i);
     tabFlag[i].html = document.createElement("div");
     att = document.createAttribute("class");
 
@@ -280,9 +397,9 @@ function init() {
       do {
         r = randInt(4);
         superSwitch(tabFlag[i], r, 8, 3, 7, false);
-      } while (tabBoard[tabFlag[i].row][tabFlag[i].cell] == true);
+      } while (tabBoard[tabFlag[i].row][tabFlag[i].cell] != false);
 
-      tabBoard[tabFlag[i].row][tabFlag[i].cell] = true;
+      tabBoard[tabFlag[i].row][tabFlag[i].cell] = i;
       (i < 6) ? att.value = "redFlag" : att.value = "blueFlag";
     }
 
@@ -293,76 +410,9 @@ function init() {
   refreshPos();
 }
 
-function show_deck(deck,id){
-  deck_ind = id;
-  document.getElementById("deck").style.display = "block";
-  document.getElementById("img_deck").innerHTML = "";
-  var html;
-  console.log(deck);
-  if (deck == 0) {
-    isRed = true;
-    html = print_deck(red_deck,temporary_red_hand);
-    document.getElementById("deck").style.backgroundColor = "#db6641";
-    document.getElementById("deck").style.border = "2px solid #a33614";
-  }else{
-    isRed = false;
-    html = print_deck(blue_deck,temporary_blue_hand);
-    document.getElementById("deck").style.backgroundColor = "#418cdb";
-    document.getElementById("deck").style.border = "2px solid #1449a3";
-  }
-  document.getElementById("img_deck").innerHTML = html;
-}
-
-function print_deck(deck,temporary_hand){
-  var html="";
-  for(var i in deck){
-    if(temporary_hand.indexOf(i) == -1){
-        html += '<input type="image" onclick="chose_card(\''+i+'\')" class="show_deck" src="'+deck[i].html+'" alt="'+i+'">';
-    }
-  }
-  return html;
-}
-
-function chose_card(ind){
-  var id;
-  var src;
-  var id_text;
-  var action;
-  if(isRed){
-    id = "red_"+deck_ind;
-    id_text = "red_text_"+deck_ind;
-    src = red_deck[ind].html;
-    temporary_red_hand[deck_ind] = ind;
-    document.getElementById("img_deck").innerHTML = "";
-    document.getElementById("img_deck").innerHTML = print_deck(red_deck,temporary_red_hand);
-  }else{
-    id = "blue_"+deck_ind;
-    id_text = "blue_text_"+deck_ind;
-    src = blue_deck[ind].html;
-    temporary_blue_hand[deck_ind] = ind;
-    document.getElementById("img_deck").innerHTML = "";
-    document.getElementById("img_deck").innerHTML = print_deck(blue_deck,temporary_blue_hand);
-  }
-  document.getElementById(id).src = src;
-  document.getElementById(id_text).style.display = "none";
-
-}
-
-function close_deck(){
-  document.getElementById("deck").style.display = "none";
-}
-
-function useCards(i, b) {
-  if (b) {
-    blueRobot.useCard(i);
-  }
-  else {
-    redRobot.useCard(i);
-  }
-
-  return !b;
-}
-
+/* main():
+ *  Main loop of the game
+ */
 function main() {
   console.log("--MAIN--");
 
@@ -390,19 +440,19 @@ function main() {
   red_deck['red_x2'] = new Card("rouge", "x2", "X2");
   red_deck['red_pause'] = new Card("rouge", "pause", "STOP");
 
-  blue_deck['blue_north'].reaction(blueRobot.tabCard, 0);
-  blue_deck['blue_west'].reaction(blueRobot.tabCard, 1);
-  blue_deck['blue_west'].reaction(blueRobot.tabCard, 2);
-  blue_deck['blue_west'].reaction(blueRobot.tabCard, 3);
-  blue_deck['blue_south'].reaction(blueRobot.tabCard, 4);
-  blue_deck['blue_east'].reaction(blueRobot.tabCard, 5);
+  blue_deck['blue_north'].addTo(blueRobot.tabCard, 0);
+  blue_deck['blue_west'].addTo(blueRobot.tabCard, 1);
+  blue_deck['blue_west'].addTo(blueRobot.tabCard, 2);
+  blue_deck['blue_west'].addTo(blueRobot.tabCard, 3);
+  blue_deck['blue_south'].addTo(blueRobot.tabCard, 4);
+  blue_deck['blue_east'].addTo(blueRobot.tabCard, 5);
 
-  red_deck['red_north'].reaction(redRobot.tabCard, 0);
-  red_deck['red_east'].reaction(redRobot.tabCard, 1);
-  red_deck['red_east'].reaction(redRobot.tabCard, 2);
-  red_deck['red_east'].reaction(redRobot.tabCard, 3);
-  red_deck['red_south'].reaction(redRobot.tabCard, 4);
-  red_deck['red_west'].reaction(redRobot.tabCard, 5);
+  red_deck['red_north'].addTo(redRobot.tabCard, 0);
+  red_deck['red_east'].addTo(redRobot.tabCard, 1);
+  red_deck['red_east'].addTo(redRobot.tabCard, 2);
+  red_deck['red_east'].addTo(redRobot.tabCard, 3);
+  red_deck['red_south'].addTo(redRobot.tabCard, 4);
+  red_deck['red_west'].addTo(redRobot.tabCard, 5);
 
   var i = 0;
   var alt = true;
@@ -418,11 +468,14 @@ function main() {
   }, 2000);
 }
 
-window.addEventListener("resize", refreshPos);
+/*---EVENT LISTENERS----------------------------------------------------------*/
 
+/* After the DOMContent is loaded */
 document.addEventListener("DOMContentLoaded", function(e) {
+  /* Initialization of the game */
   init();
 
+  /* Call refreshPos() after the animation of a robot */
   document.getElementById("blueRobot").addEventListener("animationend", function() {
     document.getElementById("blueRobot").removeAttribute("class");
     refreshPos();
@@ -433,5 +486,9 @@ document.addEventListener("DOMContentLoaded", function(e) {
     refreshPos();
   });
 
+  /* Main loop */
   main();
 });
+
+/* Every resize of the window calls refreshPos */
+window.addEventListener("resize", refreshPos);
